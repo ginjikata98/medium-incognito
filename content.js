de3271367ae22433d6071f62d7dafb9c80b4922b
Tony <minhdvu98@gmail.com>
2020-10-14 17:26:15 +0700

    use left mouse click event instead of the shitty code before

--- content.js
+++ content.js
@@ -1,33 +1,13 @@
 window.addEventListener('load', () => {
-    console.log('medium crack')
-
-    const linkCache = new Set()
-
-    setInterval(() => {
-        console.log('start reload links')
-        let newLinkCount = 0
-
-        const links = document.links
-        for (let i = 0; i < links.length; i++) {
-            const url = links[i].href
-
-            if (linkCache.has(url)) continue;
-
-            newLinkCount++
-            linkCache.add(url)
-
-            links[i].style.color = "blue";
-            links[i].addEventListener('click', (e) => {
-                e.preventDefault()
-                chrome.runtime.sendMessage({url: links[i].href}, (response) => {
-                    console.log(response);
-                });
-            })
+    document.addEventListener('mousedown', (e) => {
+        if (e.button === 2) {
+            const origin = e.target.closest('a')
+            console.log(origin.href)
+            chrome.runtime.sendMessage({url: origin.href}, (response) => {
+                console.log(response);
+            });
         }
-
-        if (newLinkCount) console.log(`${newLinkCount} is added`)
-
-    }, 500)
+    })
 })
 
 
